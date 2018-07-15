987682428a7b5860b07ddc8e0fabf4914e850753
John Else <john.else@gmail.com>
2018-07-15 04:12:23 +0100

    Send messages to xenguest

--- bin/xenguest.ml
+++ bin/xenguest.ml
@@ -2,6 +2,37 @@ let xenguest_path = "/usr/libexec/xen/bin/xenguest"
 
 let control_path domid = Printf.sprintf "/var/xen/xenguest/%d/control" domid
 
+type message =
+  | Set_args of (string * string) list
+  | Migrate_init
+  | Migrate_pause
+  | Migrate_paused
+  | Migrate_nonlive
+  | Restore
+  | Quit
+
+let send fd message =
+  (* Write the command. *)
+  let out_json = match message with
+  | Set_args args -> begin
+    let args_json = `O (List.map (fun (k, v) -> (k, `String v)) args) in
+    `O ["execute", `String "set_args"; "arguments", args_json]
+  end
+  | Migrate_init    -> `O ["execute", `String "migrate_init"]
+  | Migrate_pause   -> `O ["execute", `String "migrate_pause"]
+  | Migrate_paused  -> `O ["execute", `String "migrate_paused"]
+  | Migrate_nonlive -> `O ["execute", `String "migrate_nonlive"]
+  | Restore         -> `O ["execute", `String "restore"]
+  | Quit            -> `O ["execute", `String "quit"]
+  in
+  IO.really_write_string fd (Ezjsonm.to_string out_json);
+
+  (* Read the response. *)
+  let in_json = `O ["return", `O []] in
+  let in_string = Ezjsonm.to_string in_json in
+  let (_:string) = IO.really_read_string fd ((String.length in_string) + 1) in
+  ()
+
 type args = {
   path: string;
   args: string array;
--- bin/xenguest.mli
+++ bin/xenguest.mli
@@ -1,5 +1,18 @@
 (** Get the path to the xenguest control socket for the specified domid. *)
 val control_path : int -> string
 
+(** The type of messages which can be sent to xenguest. *)
+type message =
+  | Set_args of (string * string) list
+  | Migrate_init
+  | Migrate_pause
+  | Migrate_paused
+  | Migrate_nonlive
+  | Restore
+  | Quit
+
+(** Send a message to xenguest via the supplied socket. *)
+val send : Unix.file_descr -> message -> unit
+
 (** exec xenguest with the specified parameters. *)
 val exec : Params.params -> unit
