d1bf730df3590adbef72bc1cc1ef5e6202299dfb
John Else <john.else@gmail.com>
2018-07-16 02:12:14 +0100

    Add Control module for communicating with the controlling process

---
+++ bin/control.ml
@@ -0,0 +1,28 @@
+type out_message =
+  | Suspend
+  | Prepare
+  | Result of int * int
+
+let output_line out_chan data =
+  output_string out_chan (Printf.sprintf "%s\n" data)
+
+let send out_chan = function
+  | Suspend -> output_line out_chan "suspend:"
+  | Prepare -> output_line out_chan "prepare:xenguest"
+  | Result (xenstore_mfn, console_mfn) ->
+    output_line
+      out_chan
+      (Printf.sprintf "result:%d %d" xenstore_mfn console_mfn)
+
+let expect_done in_chan =
+  match input_line in_chan with
+  | "done" -> ()
+  | _      -> failwith "bad control message"
+
+type in_message =
+  | Restore
+
+let receive in_chan =
+  match input_line in_chan with
+  | "restore:xenguest" -> Restore
+  | _                  -> failwith "bad control message"
---
+++ bin/control.mli
@@ -0,0 +1,13 @@
+type out_message =
+  | Suspend
+  | Prepare
+  | Result of int * int
+
+val send : out_channel -> out_message -> unit
+
+val expect_done : in_channel -> unit
+
+type in_message =
+  | Restore
+
+val receive : in_channel -> in_message
